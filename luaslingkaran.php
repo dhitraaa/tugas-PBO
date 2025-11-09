97c511161cbc4964f208308810979b5e53b3eb4a
unknown <djakiadhip@gmail.com>
2025-11-09 18:43:10 +0700

    P5-construct

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -2,19 +2,27 @@
 
 class luaslingkaran {
     public const phi = 3.14;
-    public int $jari;
+    protected int $jari;
 
-    public function tampil($nama = 'ban') {
-        $rumus = luaslingkaran:: phi* $this->jari * $this->jari;
-        echo "lingkaran {$nama} Hasilnya adalah:{$rumus}";
+    public function_construct($isijari=1) {
+        $this->jari =$isijari
     }
+    public function tampil($nama = 'ban'){
+        $rumus = <luaslingkaran::pphi *($this->jari * $this->jari);
+        echo "lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
     public static function testing(){
         echo "ini dari static";
     }
+    }
+    public function_destruct(){
+        echo "udah ah cape juga";
+    }
 }
 
-$lingkaran = new luaslingkaran();
-$lingkaran->jari = 16;
+$lingkaran = new luaslingkaran(16);
+//$lingkaran->jari = 16;
 $lingkaran->tampil('roda'); //panggil method
 
 luaslingkaran::testing();
