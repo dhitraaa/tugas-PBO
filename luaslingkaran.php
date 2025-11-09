29bb217e03f72ea15f9136d846c4e9a70d3c2444
Djaki Adhi Putra <djakiadhip@gmail.com>
2025-11-09 15:23:16 +0700

    main

---
+++ luaslingkaran.php
@@ -0,0 +1,12 @@
+<?php
+
+class luaslingkaran {
+    public const phi = 3.14;
+    public int $jari;
+}
+
+$lingkaran = new luaslingkaran ();
+$lingkaran -> jari = 16;
+$rumu = luaslingakaran::phi * $lingkaran->jari * $lingkaran->jari;
+
+echo "Hasil Adalah". $rumus;
